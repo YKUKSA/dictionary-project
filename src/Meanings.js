4f9370adea8c419331863af941cdde68d1614d03
Yuliia <kuksa.yulia@gmail.com>
2022-10-30 11:05:02 +0200

    adding phonetics

--- src/Meanings.js
+++ src/Meanings.js
@@ -1,10 +1,19 @@
 import React from "react";
 import Result from "./Result";
+import Phonetic from "./Phonetic";
 export default function Meanings(props) {
+  console.log(props.meanings);
   if (props.meanings) {
     return (
       <div className="Meanings">
         <h2>{props.meanings.word}</h2>
+        {props.meanings.phonetics.map(function (phonetic, index) {
+          return (
+            <div key={index}>
+              <Phonetic phonetic={phonetic} />
+            </div>
+          );
+        })}
         {props.meanings.meanings.map(function (meaning, index) {
           return (
             <div key={index}>
--- src/Result.js
+++ src/Result.js
@@ -1,13 +1,14 @@
 import React from "react";
 import "./Result.css";
 import Synonyms from "./Synonyms";
+
 export default function Result(props) {
-  console.log(props.meaning);
   return (
     <div className="Result">
       <em>
         <h4>{props.meaning.partOfSpeech}</h4>
       </em>
+
       {props.meaning.definitions.map(function (definition, index) {
         return (
           <div key={index}>
