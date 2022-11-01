318d194f592438c2413d2b5a05817d7244ffea26
Yuliia <kuksa.yulia@gmail.com>
2022-11-01 22:07:38 +0200

    adding links in footer

--- src/App.js
+++ src/App.js
@@ -8,6 +8,24 @@ function App() {
         <p>What word do you want to look up?</p>
       </header>
       <DictionarySearch />
+      <footer className="App-footer">
+        This project was coded by Kuksa Yuliia and is {""}
+        <a
+          href="https://github.com/YKUKSA/dictionary-project"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          open-sourced on GitHub
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://bucolic-alpaca-8dc83f.netlify.app/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          hosted on Netlify
+        </a>
+      </footer>
     </div>
   );
 }
